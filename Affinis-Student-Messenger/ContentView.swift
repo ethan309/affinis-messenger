c5ac021cbb4f04e8f76967fcc07b12a795ff1aef
Ethan Butt <ethanbutt309@gmail.com>
2020-02-17 12:13:07 -0600

    Initial Commit

---
+++ Affinis-Student-Messenger/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Affinis-Student-Messenger
+//
+//  Created by Ethan Bütt on 02.17.20.
+//  Copyright © 2020 Ethan Bütt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Affinis-Student-MessengerTests/Affinis_Student_MessengerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Affinis_Student_MessengerTests.swift
+//  Affinis-Student-MessengerTests
+//
+//  Created by Ethan Bütt on 02.17.20.
+//  Copyright © 2020 Ethan Bütt. All rights reserved.
+//
+
+import XCTest
+@testable import Affinis_Student_Messenger
+
+class Affinis_Student_MessengerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
